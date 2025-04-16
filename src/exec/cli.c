f95e35413a4bc946ab05bbbb42ada2cb6de51ece
Alessandro-Salerno <OxyGen05@outlook.it>
2025-04-16 15:02:01 +0200

    fixed cli

--- src/exec/cli.c
+++ src/exec/cli.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -8,8 +9,32 @@
 #include "rarsjs/elf.h"
 #include "vendor/commander.h"
 
+#define CHK_CMD()                                         \
+    if (NULL != g_command) {                              \
+        fprintf(stderr, "only one command is allowed\n"); \
+        exit(-1);                                         \
+    }                                                     \
+    g_next_arg = malloc(strlen(self->arg) + 1);           \
+    strcpy(g_next_arg, self->arg);
+
+// Type of command handler functions (c_*)
+typedef void (*cmd_func_t)(void);
+
+// Default output paths, changed by opt_o
 static const char *g_obj_out = "a.o";
 static const char *g_exec_out = "a.out";
+static bool g_out_changed = false;
+
+// Copies of argc, argv from main
+static char **g_argv;
+static int g_argc;
+
+// The next argument in the command, set by opt_*
+// for commands c_*
+static char *g_next_arg = NULL;
+static cmd_func_t g_command = NULL;
+
+// UTILITY FUNCTIONS
 
 static void emulate_safe(void) {
     while (!g_exited) {
@@ -17,19 +42,26 @@ static void emulate_safe(void) {
 
         switch (g_runtime_error_type) {
             case ERROR_FETCH:
-                fprintf(stderr, "emulator: fetch error at pc=0x%08x on addr=0x%08x\n", g_pc, g_runtime_error_addr);
+                fprintf(stderr,
+                        "emulator: fetch error at pc=0x%08x on addr=0x%08x\n",
+                        g_pc, g_runtime_error_addr);
                 return;
 
             case ERROR_LOAD:
-                fprintf(stderr, "emulator: load error at pc=0x%08x on addr=0x%08x\n", g_pc, g_runtime_error_addr);
+                fprintf(stderr,
+                        "emulator: load error at pc=0x%08x on addr=0x%08x\n",
+                        g_pc, g_runtime_error_addr);
                 return;
 
             case ERROR_STORE:
-                fprintf(stderr, "emulator: store error at pc=0x%08x on addr=0x%08x\n", g_pc, g_runtime_error_addr);
+                fprintf(stderr,
+                        "emulator: store error at pc=0x%08x on addr=0x%08x\n",
+                        g_pc, g_runtime_error_addr);
                 return;
 
             case ERROR_UNHANDLED_INSN:
-                fprintf(stderr, "emulator: unhandled insn at pc=0x%08x\n", g_pc);
+                fprintf(stderr, "emulator: unhandled insn at pc=0x%08x\n",
+                        g_pc);
                 return;
 
             default:
@@ -59,8 +91,10 @@ static void assemble_from_file(const char *src_path, bool allow_externs) {
     }
 }
 
-static void c_build(command_t *self) {
-    assemble_from_file(self->arg, false);
+// COMMANDS
+
+static void c_build(void) {
+    assemble_from_file(g_next_arg, false);
 
     if (g_error) {
         return;
@@ -86,8 +120,8 @@ static void c_build(command_t *self) {
     fclose(out);
 }
 
-static void c_run(command_t *self) {
-    FILE *elf = fopen(self->arg, "rb");
+static void c_run(void) {
+    FILE *elf = fopen(g_next_arg, "rb");
 
     if (NULL == elf) {
         fprintf(stderr, "loader: could not open input file\n");
@@ -116,8 +150,8 @@ static void c_run(command_t *self) {
     emulate_safe();
 }
 
-static void c_emulate(command_t *self) {
-    assemble_from_file(self->arg, false);
+static void c_emulate(void) {
+    assemble_from_file(g_next_arg, false);
     if (g_error) {
         return;
     }
@@ -131,8 +165,8 @@ static void c_emulate(command_t *self) {
     emulate_safe();
 }
 
-static void c_readelf(command_t *self) {
-    FILE *elf = fopen(self->arg, "rb");
+static void c_readelf(void) {
+    FILE *elf = fopen(g_next_arg, "rb");
 
     if (NULL == elf) {
         fprintf(stderr, "readelf: could not open input file\n");
@@ -172,20 +206,27 @@ static void c_readelf(command_t *self) {
     printf(" %-35s: %s\n", "Type", readelf.type);
     printf(" %-35s: %s\n", "Architecture", readelf.architecture);
     printf(" %-35s: 0x%08x\n", "Entry point", readelf.ehdr->entry);
-    printf(" %-35s: %u (bytes into file)\n", "Start of program headers", readelf.ehdr->phdrs_off);
-    printf(" %-35s: %u (bytes into file)\n", "Start of section headers", readelf.ehdr->shdrs_off);
+    printf(" %-35s: %u (bytes into file)\n", "Start of program headers",
+           readelf.ehdr->phdrs_off);
+    printf(" %-35s: %u (bytes into file)\n", "Start of section headers",
+           readelf.ehdr->shdrs_off);
     printf(" %-35s: 0x%x\n", "Flags", readelf.ehdr->flags);
     printf(" %-35s: %u (bytes)\n", "Size of ELF header", readelf.ehdr->ehdr_sz);
-    printf(" %-35s: %u (bytes)\n", "Size of each program header", readelf.ehdr->phent_sz);
-    printf(" %-35s: %u\n", "Number of program headers", readelf.ehdr->phent_num);
-    printf(" %-35s: %u (bytes)\n", "Size of each section header", readelf.ehdr->shent_sz);
-    printf(" %-35s: %u\n", "Number of section headers", readelf.ehdr->shent_num);
-    printf(" %-35s: %u\n", "Section header string table index", readelf.ehdr->shdr_str_idx);
+    printf(" %-35s: %u (bytes)\n", "Size of each program header",
+           readelf.ehdr->phent_sz);
+    printf(" %-35s: %u\n", "Number of program headers",
+           readelf.ehdr->phent_num);
+    printf(" %-35s: %u (bytes)\n", "Size of each section header",
+           readelf.ehdr->shent_sz);
+    printf(" %-35s: %u\n", "Number of section headers",
+           readelf.ehdr->shent_num);
+    printf(" %-35s: %u\n", "Section header string table index",
+           readelf.ehdr->shdr_str_idx);
     printf("\n");
 
     printf("Section headers:\n");
-    printf(" [Nr] %-17s %-15s %-10s %-10s %-10s %-5s %-5s\n", "Name", "Type", "Address", "Offset", "Size", "Flags",
-           "Align");
+    printf(" [Nr] %-17s %-15s %-10s %-10s %-10s %-5s %-5s\n", "Name", "Type",
+           "Address", "Offset", "Size", "Flags", "Align");
 
     for (u32 i = 0; i < readelf.ehdr->shent_num; i++) {
         ReadElfSection *sec = &readelf.shdrs[i];
@@ -198,8 +239,8 @@ static void c_readelf(command_t *self) {
     printf("\n");
 
     printf("Program headers:\n");
-    printf(" %-14s %-10s %-15s %-16s %-10s %-5s %-5s\n", "Type", "Offset", "Virtual Address", "Physical Address",
-           "Size", "Flags", "Align");
+    printf(" %-14s %-10s %-15s %-16s %-10s %-5s %-5s\n", "Type", "Offset",
+           "Virtual Address", "Physical Address", "Size", "Flags", "Align");
     for (u32 i = 0; i < readelf.ehdr->phent_num; i++) {
         ReadElfSegment *seg = &readelf.phdrs[i];
         // clang-format off
@@ -211,18 +252,8 @@ static void c_readelf(command_t *self) {
     printf("\n");
 }
 
-static void c_output(command_t *self) {
-    g_obj_out = self->arg;
-    g_exec_out = self->arg;
-}
-
-static void c_assemble(command_t *self) {
-    assemble_from_file(self->arg, true);
-    /*Extern *e = push(g_externs, g_externs_len, g_externs_cap);
-    *e = (Extern){.symbol = "_test_ext", .len = strlen("_test_ext")};
-    *push(g_text.relocations.buf, g_text.relocations.len, g_text.relocations.cap) =
-        (Relocation){.type = R_RISCV_32, .symbol = e};*/
-
+static void c_assemble(void) {
+    assemble_from_file(g_next_arg, true);
     if (g_error) {
         return;
     }
@@ -247,8 +278,10 @@ static void c_assemble(command_t *self) {
     fclose(out);
 }
 
-static void c_hexdump(command_t *self) {
-    FILE *file = fopen(self->arg, "rb");
+// OPTIONS
+
+static void c_hexdump() {
+    FILE *file = fopen(g_next_arg, "rb");
 
     if (NULL == file) {
         fprintf(stderr, "hexdump: could not open file\n");
@@ -268,25 +301,82 @@ static void c_hexdump(command_t *self) {
     }
 }
 
+static void opt_assemble(command_t *self) {
+    CHK_CMD();
+    g_command = c_assemble;
+}
+
+static void opt_build(command_t *self) {
+    CHK_CMD();
+    g_command = c_build;
+}
+
+static void opt_run(command_t *self) {
+    CHK_CMD();
+    g_command = c_run;
+}
+
+static void opt_emulate(command_t *self) {
+    CHK_CMD();
+    g_command = c_emulate;
+}
+
+static void opt_readelf(command_t *self) {
+    CHK_CMD();
+    g_command = c_readelf;
+}
+
+static void opt_o(command_t *self) {
+    g_obj_out = malloc(strlen(self->arg) + 1);
+    strcpy(g_obj_out, self->arg);
+    g_exec_out = g_obj_out;
+    g_out_changed = true;
+}
+
+static void opt_hexdump(command_t *self) {
+    CHK_CMD();
+    g_command = c_hexdump;
+}
+
 int main(int argc, char **argv) {
     atexit(free_runtime);
+    g_argc = argc;
+    g_argv = argv;
+
     command_t cmd;
     // TODO: place real version number
     command_init(&cmd, argv[0], "0.0.1");
+
     command_option(&cmd, "-a", "--assemble <file>",
-                   "assemble an RV32 assembly file and output an ELF32 relocatable object file", c_assemble);
+                   "assemble an RV32 assembly file and output an ELF32 "
+                   "relocatable object file",
+                   opt_assemble);
     command_option(&cmd, "-b", "--build <file>",
                    "assemble an RV32 assembly file"
                    " and output an ELF32 executable",
-                   c_build);
-    command_option(&cmd, "-r", "--run <file>", "run an ELF32 executable", c_run);
-    command_option(&cmd, "-e", "--emulate <file>", "assemble and run an RV32 assembly file", c_emulate);
-    command_option(&cmd, "-i", "--readelf <file>", "show information about ELF file", c_readelf);
-    command_option(&cmd, "-o", "--output <file>", "choose output file name", c_output);
-    command_option(&cmd, "-x", "--hexdump <file>", "perform hexdump of file", c_hexdump);
+                   opt_build);
+    command_option(&cmd, "-r", "--run <file>", "run an ELF32 executable",
+                   opt_run);
+    command_option(&cmd, "-e", "--emulate <file>",
+                   "assemble and run an RV32 assembly file", opt_emulate);
+    command_option(&cmd, "-i", "--readelf <file>",
+                   "show information about ELF file", opt_readelf);
+    command_option(&cmd, "-o", "--output <file>", "choose output file name",
+                   opt_o);
+    command_option(&cmd, "-x", "--hexdump <file>", "perform hexdump of file",
+                   opt_hexdump);
     command_parse(&cmd, argc, argv);
+    command_free(&cmd);
 
-    if (1 == argc) {
+    if (1 == argc || NULL == g_command) {
         command_help(&cmd);
+        return EXIT_FAILURE;
     }
+
+    g_command();
+    free((void *)g_next_arg);
+    if (g_out_changed) {
+        free((void *)g_obj_out);
+    }
+    return EXIT_SUCCESS;
 }
